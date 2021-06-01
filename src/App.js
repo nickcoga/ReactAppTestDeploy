52879b35bdb4bf0f04cb81669751b87e6c46e97a
Nick Correa <nickcorreagarcia@gmail.com>
2021-06-01 15:43:01 -0500

    Update App component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Full Stack Developer Nick C. G</p>
+        <p>Full Stack Web Developer Nick C. G</p>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
